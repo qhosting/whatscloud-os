b45e510d1e0e607268198082b56170edffe22227
qhosting <admin@qhosting.net>
2026-03-18 20:50:15 -0600

    chore: remove old chatcenter PHP codebase

--- chatcenter/cms/views/pages/custom/archivos/content/modules/drag_drop/drag_drop.php
+++
@@ -1,9 +0,0 @@
-<!--=====================================
-DRAG & DROP
-======================================-->
-
-<div class="container-fluid mb-3 px-1">
-	<div class="jumbotron p-5 text-center rounded" id="dragFiles">
-		<h4>¡Arrastra archivos aquí!</h4>
-	</div>		
-</div>
